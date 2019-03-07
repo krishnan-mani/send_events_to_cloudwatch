b8c75cf43e4158a6250a6559b9a4978b881af89b
Krishnan M <km@krishnanm.com>
2019-03-07 10:24:12 +0000

    Timestamps are a minute apart starting 50 minutes in the past

--- lib/log_events_generator.rb
+++ lib/log_events_generator.rb
@@ -1,6 +1,10 @@
 class LogEventsGenerator
 
-  MAXIMUM_LOG_EVENTS_PER_BATCH = 10000
+  ONE_MINUTE_AS_SECONDS = 60
+  MINUTES_OFFSET = 50
+  TIME_OFFSET = MINUTES_OFFSET * ONE_MINUTE_AS_SECONDS
+
+  MAXIMUM_LOG_EVENTS_PER_BATCH = MINUTES_OFFSET - 10
 
   attr_reader :log_line, :pattern
 
@@ -11,13 +15,21 @@ class LogEventsGenerator
   end
 
   def get_log_events(number_of_log_events, min_value, max_value)
-    seed_timestamp_milliseconds = Time.now.to_i * 1000
+    start_timestamp_milliseconds = get_start_timestamp_milliseconds
     number_of_log_events = [number_of_log_events, MAXIMUM_LOG_EVENTS_PER_BATCH].min
-    log_events = 1.upto(number_of_log_events).collect {|n| LogEvent.new(seed_timestamp_milliseconds + n, "#{get_message(min_value, max_value)}").to_event}
-    {log_stream_name: "synthetic-#{seed_timestamp_milliseconds}", log_events: log_events}
+    log_events = 1.upto(number_of_log_events).collect {|n| LogEvent.new(get_next_minute_timestamp(n, start_timestamp_milliseconds), "#{get_message(min_value, max_value)}").to_event}
+    {log_stream_name: "synthetic-#{start_timestamp_milliseconds}", log_events: log_events}
   end
 
   private
+
+  def get_next_minute_timestamp(n, start_timestamp_milliseconds)
+    start_timestamp_milliseconds + (n * ONE_MINUTE_AS_SECONDS * 1000)
+  end
+
+  def get_start_timestamp_milliseconds
+    (Time.now - TIME_OFFSET).to_i * 1000
+  end
 
   def get_message(min_value, max_value)
     log_line.gsub(pattern, @random.rand(min_value..max_value).to_s)
